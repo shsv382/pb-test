4ced19e110f5328a89cccad035d1ab034cd4c871
Sergey Shabanov <shsv382@gmail.com>
2025-02-15 10:19:26 +0300

    Made pretty

--- src/components/OrganizationTree.vue
+++ src/components/OrganizationTree.vue
@@ -28,11 +28,11 @@
         autofocus
         />
     </template> 
-        <OrganizationTree
-          v-if="division.children.length"
-          :organization="division"
-          :root="false"
-        />
+      <OrganizationTree
+        v-if="division.children.length"
+        :organization="division"
+        :root="false"
+      />
   </el-sub-menu>
 </template>
 
@@ -41,44 +41,44 @@ import { defineProps } from 'vue';
 import { RouterLink } from 'vue-router';
 
 const props = defineProps<{
-    organization: Division;
-    root?: boolean
+  organization: Division;
+  root?: boolean
 }>();
 
 interface Officer {
-    id: number;
-    firstName: string;
-    lastName: string;
-    division: Division;
+  id: number;
+  firstName: string;
+  lastName: string;
+  division: Division;
 }
 
 interface Division {
-    id: number;
-    name: string;
-    children: Division[];
-    isOpen?: boolean; 
-    isEditing?: boolean;
+  id: number;
+  name: string;
+  children: Division[];
+  isOpen?: boolean; 
+  isEditing?: boolean;
 }
 
 // Функция для раскрытия/сворачивания узла
 const toggleDivision = (division: Division) => {
-    division.isOpen = !division.isOpen;
+  division.isOpen = !division.isOpen;
 };
 
 // Функция для начала редактирования
-    const startEditing = (division: Division) => {
-    division.isEditing = true;
-    };
+const startEditing = (division: Division) => {
+  division.isEditing = true;
+};
 
-    // Функция для завершения редактирования
-    const stopEditing = (division: Division) => {
-    division.isEditing = false;
-    };
+// Функция для завершения редактирования
+const stopEditing = (division: Division) => {
+  division.isEditing = false;
+};
 
-    // Функция для отмены редактирования
-    const cancelEditing = (division: Division) => {
-    division.isEditing = false;
-    };
+// Функция для отмены редактирования
+const cancelEditing = (division: Division) => {
+  division.isEditing = false;
+};
 </script>
 
 <style lang="scss">
