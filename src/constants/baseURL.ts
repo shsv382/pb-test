516a3ba41af8b7ee91139aab334dc564cd87f088
Sergey Shabanov <shsv382@gmail.com>
2025-02-22 14:36:14 +0300

    Bug fixed

--- src/constants/baseURL.ts
+++ src/constants/baseURL.ts
@@ -3,7 +3,7 @@ let baseURL: string = 'http://localhost:4000'
 if (process.env.NODE_ENV === 'development') {
     baseURL = 'http://localhost:4000'
 } else if (process.env.NODE_ENV === 'production') {
-    baseURL = 'http://89.22.226.203:4000/'
+    baseURL = 'http://89.22.226.203:4000'
 }
   
 export { baseURL }
