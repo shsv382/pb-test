a4921c4867804c2ff908ed91d640f0939bcff924
Sergey Shabanov <shsv382@gmail.com>
2025-02-22 14:34:48 +0300

    Конфигурация Docker для запуска

--- src/constants/baseURL.ts
+++ src/constants/baseURL.ts
@@ -3,7 +3,7 @@ let baseURL: string = 'http://localhost:4000'
 if (process.env.NODE_ENV === 'development') {
     baseURL = 'http://localhost:4000'
 } else if (process.env.NODE_ENV === 'production') {
-    baseURL = 'http://backend:4000/api'
+    baseURL = 'http://89.22.226.203:4000/'
 }
   
 export { baseURL }
