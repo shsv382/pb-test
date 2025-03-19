61a2d23010309809c14923e02bc705e9a6394ac9
Sergey Shabanov <shsv382@gmail.com>
2025-03-19 10:15:40 +0300

    Добавлены стили страницы входа

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,5 +5,12 @@
 </template>
 
 <style scoped lang="scss">
-
+.login {
+  width: 100vw;
+  height: 100vh;
+  background-image: url(../assets/svg/background.svg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 </style>
